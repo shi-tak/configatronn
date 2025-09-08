db1b2e6d587f40aa2cfea69614417acaf154720f
Taku Shimizu <t_shimizu@soloshmz.com>
2025-09-08 21:14:07 +0900

    fix bug: Configatron::Integrations::Rails.init raise without Rails gem

--- lib/configatron/integrations/rails.rb
+++ lib/configatron/integrations/rails.rb
@@ -19,12 +19,12 @@ module Configatron::Integrations
     def self.init(root = nil, env = nil)
       base_dir = root
       if root.nil?
-        root = defined?(Rails) ? ::Rails.root : FileUtils.pwd
+        root = defined?(::Rails) ? ::Rails.root : FileUtils.pwd
         base_dir = File.expand_path(File.join(root, 'config', 'configatron'))
       end
 
       if env.nil?
-        env = defined?(Rails) ? ::Rails.env : 'development'
+        env = defined?(::Rails) ? ::Rails.env : 'development'
       end
 
       config_files = []
