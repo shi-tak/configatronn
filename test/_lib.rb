5d6578f86cba399775892a398793b52c2b069b8a
Taku Shimizu <t_shimizu@soloshmz.com>
2025-09-08 21:35:19 +0900

    fix bug

--- test/_lib.rb
+++ test/_lib.rb
@@ -4,12 +4,12 @@ require 'bundler/setup'
 ENV['MT_NO_EXPECTATIONS'] = 'true'
 require 'minitest/autorun'
 require 'minitest/spec'
-require 'mocha/setup'
+require 'mocha/minitest'
 
 require 'configatron'
 
 module Critic
-  class Test < ::MiniTest::Spec
+  class Test < ::Minitest::Spec
     def setup
       # Put any stubs here that you want to apply globally
     end
